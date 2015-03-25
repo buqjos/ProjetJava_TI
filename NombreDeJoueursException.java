62f616751f880d4f352930fbbcf6991b42458786
buqjos <joseph.buquet@ensta-bretagne.org>
2015-03-25 09:58:06 +0100

    Validation intermédiaire

--- NombreDeJoueursException.java
+++
@@ -1,8 +0,0 @@
-
-public class NombreDeJoueursException extends Exception
-{
-	public NombreDeJoueursException()
-	{
-	    System.out.println("Vous ne pouvez saisir un nombre de joueurs négatif !");
-	}
-}
